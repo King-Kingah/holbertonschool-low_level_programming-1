bc1531958f284d5fac4393336f70080bea37842e
Elaine Yeung <yeung.egs@gmail.com>
2017-02-10 07:30:05 +0000

    Add Task 4 - v2

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,19 +9,19 @@ int main(void)
 	char c;
 	char C;
 
-	c = 'a';
-	c = 'A';
+	c = 'a'; /*declare char c to be 'a'*/
+	C = 'A'; /*declare char C to be 'A'*/
 
-	while (c <= 'z')
+	while (c <= 'z') /*while 'a' less than 'z'*/
 {
-		putchar (c);
-		c++;
+	putchar (c); /*output a char to stdout */
+	c++; /*increment this operand by one until reaching z*/
 	}
-	while (C <= 'Z')
+	while (C <= 'Z') /*while 'A' less than 'Z'*/
 	{
-		putchar (C);
-		C++;
+		putchar (C); /*output a char to stdout */
+		C++; /*increment this operand by one char until reaching 'Z'*/
 	}
-	putchar ('\n');
+	putchar ('\n'); /*add newline at end*/
 	return (0);
 }
