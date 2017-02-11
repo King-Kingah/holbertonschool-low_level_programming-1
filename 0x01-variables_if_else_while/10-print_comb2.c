56887fc0abfe8a8238a86486e3059f048ca6f80e
Elaine Yeung <yeung.egs@gmail.com>
2017-02-11 02:55:27 +0000

    Updated Task 10 - v4

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -14,14 +14,14 @@ int main(void)
 	while (a <= 9) /*as long as tens column less than or equal to 9*/
 	{
 		b = 0; /*start with 0 in ones column*/
-		while (b <= 9) /*run as long as ones column less than equal to 9*/
+		while (b <= 9) /*runs when ones column less than equal to 9*/
 		{
 			putchar('0' + a); /*output char beginning with 0*/
 			putchar('0' + b); /*output char beginning with 0*/
-			if (a != 9 || b != 9) /*if ones or tens does not equal 9*/
+			if (a != 9 || b != 9) /*if ones/ does not equal 9*/
 			{
-				putchar(','); /*...then print a comma */
-				putchar(' '); /*and a space until if statement met*/
+				putchar(','); /*print a comma */
+				putchar(' '); /*print a space*/
 			}
 			b++;
 		}
