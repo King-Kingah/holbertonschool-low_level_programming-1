3b05d14d34152dd5ebfceb44be32e31229837bb2
Elaine Yeung <yeung.egs@gmail.com>
2017-02-12 23:00:43 +0000

    Updated Task 100 documentation

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - loop through variables a and b, printing digits
  *
- * Return: always 0 (success)
- */
+ * Return: print to stdout all possible different combinations of two digits
+*/
 int main(void)
 {
-	int a = '0';
-	int b;
+	int a = '0'; /*declare variable a as integer and equal to 0*/
+	int b; /*declare variable b as integer*/
 
-	while (a <= '9')
+	while (a <= '9') /*run following while a less than or equal to 9*/
 	{
-		b = a + 1;
-		while (b <= '9')
+		b = a + 1; /*b is always one more than a*/
+		while (b <= '9') /*run following until b equal to 9*/
 		{
-			putchar(a);
-			putchar(b);
-			if (a == '8' && b == '9')
+			putchar(a); /*print character a*/
+			putchar(b); /*print character b*/
+			if (a == '8' && b == '9') /*if a=8 AND b=9*/
 			{
-				putchar('\n');
+				putchar('\n'); /*then print a newline*/
 			}
-			else
+			else /*do following when if statement not met*/
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(','); /*print comma*/
+				putchar(' '); /*print space*/
 			}
-			b++;
+			b++; /*increment this loop by 1*/
 		}
-		a++;
+		a++; /*increment this loop by 1*/
 	}
 	return (0);
 }
