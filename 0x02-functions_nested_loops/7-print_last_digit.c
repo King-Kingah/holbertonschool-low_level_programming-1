a1740aa4415382065d49e6689ad5df06b643b697
Elaine Yeung <yeung.egs@gmail.com>
2017-02-15 04:03:51 +0000

    Task 7 - v1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ *print_last_digit - function to determine last digit
+ *@n: integer
+ *
+ *Return: the value of the last digit
+ */
+int print_last_digit(int n)
+{
+	if (n > 0)
+		return (n % 10);
+	else
+		return (n % 10);
+}
