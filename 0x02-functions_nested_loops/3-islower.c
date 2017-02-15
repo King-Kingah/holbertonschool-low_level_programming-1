936604b610f7f70423227b4a872e7cbce8de5668
Elaine Yeung <yeung.egs@gmail.com>
2017-02-15 00:14:58 +0000

    Add Task 3 - v1

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- * _islower - check for lowercase character
- *
- * return: check c for lowercase
+ * _islower- check c lowercase character
+ * @c: string
+ * Return: return 1 if c lowercase, 0 if not
  */
 int _islower(int c)
 {
-	if ( c <= 'z' && c >= 'a')
-		return(1);
-	return(0);
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	return (0);
 }
