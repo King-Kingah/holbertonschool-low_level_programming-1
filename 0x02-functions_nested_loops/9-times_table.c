96b9882d09882cef1eed3dbe4576f086238b9f9a
Elaine Yeung <yeung.egs@gmail.com>
2017-02-15 04:04:16 +0000

    Add Task 9 - v1

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+
+/**
+ * times_table - create a table of multiplication facts
+ *
+ * Return: print to stdout the 9 times table, starting with 0.
+ */
+void times_table(void)
+{
+}
