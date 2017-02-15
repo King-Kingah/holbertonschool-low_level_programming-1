a6158adc65195c1730ed175fdafdda506f002ebd
Elaine Yeung <yeung.egs@gmail.com>
2017-02-15 21:56:04 +0000

    Add Task 9 - v1

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,9 +2,35 @@
 
 /**
  * times_table - create a table of multiplication facts
+ * @int: integer
  *
  * Return: print to stdout the 9 times table, starting with 0.
  */
 void times_table(void)
 {
+       	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			if ( x * y / 10 == 0 )
+			{
+				_putchar( x * y + '0' );
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+
+			}
+			else
+			{
+				_putchar( x * y / 10 + '0');
+				_putchar( x * y % 10 + '0' );
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
