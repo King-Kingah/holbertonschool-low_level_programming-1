3b90d2e2e68b93a43da060a814a234a70a8ef1f4
Elaine Yeung <yeung.egs@gmail.com>
2017-02-16 21:00:49 +0000

    Add Task 6 - v1

---
+++ 0x03-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *print_line - d
+ *@n - integer
+ *
+ *Return - print line n spaces long to stdout; print newline if n<=0
+ */
+
+void print_line(int n)
+{
+	int x;
+
+	if (n>0)
+	{
+		for(x=0; x < n; x++)
+			_putchar('_');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
