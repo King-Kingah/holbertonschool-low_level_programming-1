851bab244c23a0d1650dfe974c66586f0325cbf4
Elaine Yeung <yeung.egs@gmail.com>
2017-02-16 20:26:42 +0000

    Add Task 3 - v1

---
+++ 0x03-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - prints numbers 0 to 9
+ *@int: integer
+ */
+void print_numbers(a)
+{
+	for(a < '0'; a <= '9'; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+}
