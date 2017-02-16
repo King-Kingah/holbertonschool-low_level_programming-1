679da8b78096e4e8799fbb0ddd1d6343e286ec5c
Elaine Yeung <yeung.egs@gmail.com>
2017-02-16 20:36:06 +0000

    Add Task 4 - v1

---
+++ 0x03-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_most_numbers - prints numbers except 2 and 4
+ *@void: no argments
+ *
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for(c=0; c <= 9; c++)
+	{
+		if ( c != 2 && !=4 )
+		_putchar(c);
+		_putchar('\n');
+	}
+}
