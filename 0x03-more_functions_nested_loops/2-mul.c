7d093b1c2a212373bb1037eb19dbfd40c07ab465
Elaine Yeung <yeung.egs@gmail.com>
2017-02-16 20:20:09 +0000

    Add Task 2 - v1

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *mul - multiples two integers
+ *@a: integer
+ *@b: integer
+ *
+ *Return: product of two integers
+ */
+int mult(int a, int b);
+{
+	return(a * b);
+}
