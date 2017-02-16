8607af84fcf90917f8969393c7f00600a486ee09
Elaine Yeung <yeung.egs@gmail.com>
2017-02-16 23:31:45 +0000

    Add Task 9 - v3

--- 0x03-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,21 +5,21 @@
  *
  *return: print to stdout numbers 1 to 100 except:
  *replace multiples of three print Fizz
- *replace multiples of four print Buzz
+ *replace multiples of five print Buzz
  */
 int main(void)
 {
 	int a;
 
-	for(a = 0; a<=100; a++)
+	for(a = 1; a<=100; a++)
 	{
 		if((a % 3) == 0)
 		{
-			printf("Fizz");
+			printf("Fizz ");
 		}
 		else if((a % 5) == 0)
 		{
-			printf("Buzz");
+			printf("Buzz ");
 		}
 		else
 		{
