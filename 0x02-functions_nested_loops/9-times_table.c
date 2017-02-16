1ae171479999bd302fadee16fcb03b1efa188a39
Elaine Yeung <yeung.egs@gmail.com>
2017-02-16 07:31:16 +0000

    Add Task 9 - v3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,44 +12,26 @@ void times_table(void)
 
 	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9; y++)
+		y = 1;
+		_putchar('0');
+		for (y = 2; y <= 9; y++)
 		{
 			if ((x * y) <= 9)
 			{
-
-				_putchar(x * y + '0');
 				_putchar(',');
 				_putchar(' ');
-			}
-			else if (x * y / 10 == 0)
-			{
+				_putchar(' ');
 				_putchar(x * y + '0');
-				_putchar(',');
-				_putchar('x');
-				}
+			}
 			else
 			{
+				_putchar(',');
 				_putchar(' ');
 				_putchar(x * y / 10 + '0');
 				_putchar(x * y % 10 + '0');
-				if (y >= 9)
-				{
-					break;
-				}
-				_putchar(',');
-				_putchar(' ');
 			}
-		}
-/**		if (y == 9 && x * y / 10 > 0)
-		{
-			_putchar(x * y / 10 + '0');
-			_putchar(x * y % 10 + '0');
 
 		}
-		else
-		{
-			_putchar(x * y + '0');
-			}**/
 		_putchar('\n');
 	}
 }
