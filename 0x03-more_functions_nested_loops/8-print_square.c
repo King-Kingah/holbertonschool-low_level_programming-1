533337d8fb373a0792b7676b69fbf3b52e44cc5e
Elaine Yeung <yeung.egs@gmail.com>
2017-02-17 00:13:17 +0000

    Add Task 8 - v1

---
+++ 0x03-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ *print_square - print squares of a specified size
+ *@size: integer
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	if (size > 0)
+	{
+		while(x < size)
+		{
+			x = 0;
+			while(y < size)
+			{
+				_putchar('#');
+				y++;
+			}
+			_putchar("\n");
+			x++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
