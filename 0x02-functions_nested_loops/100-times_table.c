14b19fe714efafa714655f42bb0dc1e7c5b88eb9
Elaine Yeung <yeung.egs@gmail.com>
2017-02-18 21:29:16 +0000

    Add Task 100 - v2

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,32 +7,41 @@ void print_times_table(int n)
 {
 	int i;
 	int j;
+	int k;
 
-	for (i = 0; i <= n; i++)
+	if (n > 0 && n < 15)
 	{
-		j = 0;
-		_putchar('0');
-		if (n < 0 || n > 15)
+		for (i = 0; i <= n; i++)
 		{
-			_putchar('\n');
-		}
+			_putchar('0');
 		for (j = 1; j <= n; j++)
 		{
-			if ((i * j) <= 9)
+			k = i * j;
+			if (k <= 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(i * j + '0');
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else if (k > 9 && k <= 99)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
 			}
 			else
 			{
 				_putchar(',');
-				_putchar(' ');
-				_putchar(i * j / 10 + '0');
-				_putchar(i * j % 10 + '0');
+				_putchar(k / 100 + '0');
+				_putchar(k / 10 % 10 + '0');
+				_putchar(k % 10 + '0');
 			}
 		}
 		_putchar('\n');
+		}
 	}
 }
