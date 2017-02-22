82d943686d7f0189124f8b952931db205bdf4cff
Elaine Yeung <yeung.egs@gmail.com>
2017-02-22 07:31:35 +0000

    Add Task 7 - v2

--- 0x04-pointers_arrays_strings/7-puts_half.c
+++ 0x04-pointers_arrays_strings/7-puts_half.c
@@ -8,9 +8,10 @@ void puts_half(char *str)
 {
 	int i;
 
-	for (i = 0; *(str + i) != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 	}
+	i = i + 1;
 	for (i = i / 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
