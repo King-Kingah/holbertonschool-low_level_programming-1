007c786f73e8019bc1886b9261306fec964e5bea
Elaine Yeung <yeung.egs@gmail.com>
2017-02-24 07:24:09 +0000

    Add Task 8 - v1

--- 0x05-pointers_arrays_strings/8-rot13.c
+++ 0x05-pointers_arrays_strings/8-rot13.c
@@ -1,9 +1,25 @@
 #include "holberton.h"
 /**
- *rot13 - function that encodes a string using rot13
- *@* - character
+ * rot13 - function that encodes a string using rot13
+ * @str: character
+ * Return: string in rot13
  */
-char *rot13(char *)
+char *rot13(char *str)
 {
+	int i;
+	int j;
+	char from[] = "abcdefghijklmABCDEFGHIJKL";
+	char to[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; from[j] != '\0'; j++)
+		{
+			if (str[i] == from[j])
+			{
+			str[i] = to[j];
+			}
+		}
+	}
+	return (str);
 }
