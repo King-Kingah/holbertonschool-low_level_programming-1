5af2a44ef0e7bdae79bcab87b561b614169b1825
Elaine Yeung <yeung.egs@gmail.com>
2017-02-24 06:30:17 +0000

    Add Task 4 - v1

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ *reverse_array: this function reverses the content of an array of integers
+ *@a: array of integers
+ *n: the number of elements of the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int temp = 0;
+
+	n = n - 1;
+	for (i = 0; i < n / 2; i++)
+	{
+		temp = a[i];
+		a[i] = a[n - i];
+		a[n - i] = temp;
+	}
+}
