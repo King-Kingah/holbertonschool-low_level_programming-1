2df964d642ab32555160a74840dd7904e039d734
Elaine Yeung <yeung.egs@gmail.com>
2017-02-24 07:03:39 +0000

    Add Task 2 - v2 - remove extra comments

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -22,17 +22,3 @@ char *_strncpy(char *dest, char *src, int n)
 
 	return (dest);
 }
-/**
- * char *strncpy(char *dest, const char *src, size_t n)
- *{
- *    size_t i;
- *
- *    for (i = 0; i < n && src[i] != '\0'; i++)
- *        dest[i] = src[i];
- *    for ( ; i < n; i++)
- *        dest[i] = '\0';
- *
- *    return dest;
- *}
- *
-*/
