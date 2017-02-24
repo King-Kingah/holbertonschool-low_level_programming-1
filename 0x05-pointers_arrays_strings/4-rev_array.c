200d3d3e27c57ae1f62030daaee946b2f06e869d
Elaine Yeung <yeung.egs@gmail.com>
2017-02-24 07:08:59 +0000

    Add Task 4 - v2 - fix formatting

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
-
 /**
- *reverse_array: this function reverses the content of an array of integers
+ *reverse_array -  this function reverses the content of an array of integers
  *@a: array of integers
- *n: the number of elements of the array
+ *@n: the number of elements of the array
  */
 void reverse_array(int *a, int n)
 {
