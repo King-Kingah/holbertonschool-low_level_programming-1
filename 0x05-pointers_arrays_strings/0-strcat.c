3912741c0a57a6d71d36a433ff425db2fac4c642
Elaine Yeung <yeung.egs@gmail.com>
2017-02-24 01:18:40 +0000

    Add Task 1 - v1

--- 0x05-pointers_arrays_strings/0-strcat.c
+++ 0x05-pointers_arrays_strings/0-strcat.c
@@ -1,1 +1,23 @@
-#include
+#include "holberton.h"
+
+/**
+ * _strcat - concatenates two strings
+ *@dest: character string destination
+ *@src: character string source
+ *
+ *Return: destination
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+	}
+
+	return (dest);
+}
