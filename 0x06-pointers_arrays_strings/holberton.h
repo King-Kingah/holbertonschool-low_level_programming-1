fc4b7510c0ac601ccfba253536709e640aafc4d2
Elaine Yeung <yeung.egs@gmail.com>
2017-02-27 23:49:11 +0000

    Add Task 3 - v1

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,18 +2,27 @@
  * _putchar
  */
 int _putchar(char c);
+
 /**
  * Write a function that fills memory with a constant byte.
  */
 char *_memset(char *s, char b, unsigned int n);
+
 /**
  * Write a function that copies memory area.
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
+
 /**
  * Write a function that locates a character in a string.
  */
 char *_strchr(char *s, char c);
+
+/**
+ * Write a function that gets the length of a prefix substring.
+ */
+unsigned int _strspn(char *s, char *accept);
+
 /**
  * Write a function that searches a string for any of a set of bytes.
  */
