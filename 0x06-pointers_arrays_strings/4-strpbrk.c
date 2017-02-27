dcd05eb3af895feecfaa33a118561c52c1eac7ea
Elaine Yeung <yeung.egs@gmail.com>
2017-02-27 23:55:38 +0000

    Update Task 2 and Task 4

---
+++ 0x06-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * _strpbrk - function searches string for any set of bytes
+ * @s: character s
+ * @accept: character accept
+ * Returns : a pointer to the byte in s
+ */
+
+char *_strpbrk(char *s, char *accept)
