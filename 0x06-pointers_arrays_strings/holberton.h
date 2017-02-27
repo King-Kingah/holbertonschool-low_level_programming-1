fa41a9d0149008dafd630feefaee739312bd62d1
Elaine Yeung <yeung.egs@gmail.com>
2017-02-27 22:02:25 +0000

    Add Holberton header

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,32 @@
+/**
+ * _putchar
+ */
+int _putchar(char c);
+/**
+ * Write a function that fills memory with a constant byte.
+ */
+char *_memset(char *s, char b, unsigned int n);
+/**
+ * Write a function that copies memory area.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
+/**
+ * Write a function that locates a character in a string.
+ */
+char *_strchr(char *s, char c);
+/**
+ * Write a function that searches a string for any of a set of bytes.
+ */
+char *_strpbrk(char *s, char *accept);
+/**
+ * Write a function that locates a substring.
+ */
+char *_strstr(char *haystack, char *needle);
+/**
+ * Write a function that prints the sum of the two diagonals of a square matrix of integers.
+ */
+void print_diagsums(int *a, int size);
+/**
+ * Write a function that sets the value of a pointer to a char.
+ */
+void set_string(char **s, char *to);
