93daa3d5acf752273ca0a54396a2bca1d926111b
Elaine Yeung <yeung.egs@gmail.com>
2017-02-28 03:24:31 +0000

    Add Task 5 - v1

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strstr - this function locates a substring
+ * @haystack:character string
+ * @needle:character string
+ * Return: a pointer to the beginning of the located substring,
+ *or NULL if the substring is not found.
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+
+	for ()
+	{
+	}
+	return (0);
+}
