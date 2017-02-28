8d5d123f785f62f971505fdfd19428e82dfdf7f3
Elaine Yeung <yeung.egs@gmail.com>
2017-02-28 07:53:02 +0000

    Add Task 5 - after whiteboarding

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -9,9 +9,22 @@
 char *_strstr(char *haystack, char *needle)
 {
 	int i;
+	int j;
+	int k;
 
-	for ()
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
+		for (k = i, j = 0; needle[j] != '\0'; j++, k++)
+		{
+			if (needle[j] != haystack[k] || haystack[k] == '\0')
+			{
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
 	}
 	return (0);
 }
