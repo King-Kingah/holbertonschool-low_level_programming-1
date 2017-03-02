e826d065f090fbde24985798f1f797546ebdc13f
Elaine Yeung <yeung.egs@gmail.com>
2017-03-02 03:47:54 +0000

    Add Task 4 - v2

--- 0x07-recursion/4-pow_recursion.c
+++ 0x07-recursion/4-pow_recursion.c
@@ -12,8 +12,12 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
+	else if (y == 0)
+	{
+		return (1);
+	}
 	else
 	{
-		_pow_recursion(x ^ y);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
