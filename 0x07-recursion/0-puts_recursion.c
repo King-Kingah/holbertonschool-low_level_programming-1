75cabcdfd5baa4c47e4cb0312f371c0b892a7c73
Elaine Yeung <yeung.egs@gmail.com>
2017-03-02 02:04:23 +0000

    Add Task 0 - v2

--- 0x07-recursion/0-puts_recursion.c
+++ 0x07-recursion/0-puts_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _puts_recursion - prints a string
  * @s: string
- * Return: string
+ * Return: void
  */
 
 void _puts_recursion(char *s)
@@ -11,7 +11,11 @@ void _puts_recursion(char *s)
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
 }
