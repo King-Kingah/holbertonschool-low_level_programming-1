8311db44532da21105bb8233e5ca9465851256e1
Elaine Yeung <yeung.egs@gmail.com>
2017-03-02 07:27:04 +0000

    Add Task 7 - v1

--- 0x07-recursion/7-is_palindrome.c
+++ 0x07-recursion/7-is_palindrome.c
@@ -5,3 +5,21 @@
  * @s: character string
  * Return 1 if string is a palindrome, and 0 if not
  */
+int is_palindrome(char *s)
+{
+	int i = 0;
+	int leftIndex = 0;
+	int rightIndex = i - 1;
+
+	if (s == '\0' || leftIndex < 0 || rightIndex < 0)
+	{
+		return (0);
+	}
+	else if (leftIndex >= rightIndex)
+		return (1);
+	if (s[leftIndex] == s[rightIndex])
+	{
+		return is_palindrome(s);
+	}
+	return (5);
+}
