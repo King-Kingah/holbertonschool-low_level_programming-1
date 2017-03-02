207f1005180ebbd862f5e8c26a94c81e67a530cf
Elaine Yeung <yeung.egs@gmail.com>
2017-03-02 07:45:01 +0000

    Add Task 6 - v2

--- 0x07-recursion/6-is_prime_number.c
+++ 0x07-recursion/6-is_prime_number.c
@@ -8,3 +8,19 @@ int is_prime_number(int n)
 {
 	return (n);
 }
+
+/**
+ * prime_or_not - helper function to support is_prime_number function
+ * @j: integer
+ * @k: integer
+ * Return: 1 if integer is prime, 0 if not
+ */
+int prime_or_not(int j, int k)
+{
+	if (j == 1 || j < 0)
+		return (0);
+	if (j == k)
+		return (1);
+	else
+		return (prime_or_not(j, k + 1));
+}
