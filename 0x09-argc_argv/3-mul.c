4a78dacc2a527a76b9e9d1e859296e4895405261
Elaine Yeung <yeung.egs@gmail.com>
2017-03-05 04:54:30 +0000

    Add Task 3 - v3 - fixed formatting

--- 0x09-argc_argv/3-mul.c
+++ 0x09-argc_argv/3-mul.c
@@ -1,19 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ *main - multiply two integers
+ *@argc: argument count
+ *@argv: argument array
+ *Return: product of two arguments, return 1 for error
+ */
 
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	int i;
-
-	if(argc != 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
 	else
 	{
-		i = atoi(argv[1]) * atoi(argv[2]);
-		printf("%d\n", i);
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	}
 	return (0);
 }
