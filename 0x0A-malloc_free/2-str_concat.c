a54ce3537ef1e542ecbabe57520f0c06fbe2018d
Elaine Yeung <yeung.egs@gmail.com>
2017-03-08 23:13:01 +0000

    Add Task 2 - testing s2[d] array

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -29,12 +29,12 @@ char *str_concat(char *s1, char *s2)
 	for (j = 0; s2[j] != '\0'; j++)
 		;
 	a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
-	for (c = 0, d = 2; c < (i + j + 1); c++)
+	for (c = 0, d = 0; c < (i + j + 1); c++)
 	{
 		if (c < i)
 			a[c] = s1[c];
 		else
-			a[c] = s2[d];
+			a[c] = s2[d + 1];
 	}
 	return (a);
 }
