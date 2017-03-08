9800853d1f929312e3015954b1f2b10314366cac
Elaine Yeung <yeung.egs@gmail.com>
2017-03-08 07:29:06 +0000

    Add Task 5 - partially complete

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * argstostr - concatenate all arguments
+ * @ac: argument count
+ * @av: double pointer to array of strings passed to main
+ * Return: NULL if fail, else return pointer to new string
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	char *p;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+	p = malloc(i * sizeof(char));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	return (p);
+}
