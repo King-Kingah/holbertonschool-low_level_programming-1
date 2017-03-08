6d2932117317c48dd5983faf59f600b8e6b572df
Elaine Yeung <yeung.egs@gmail.com>
2017-03-08 04:39:47 +0000

    Add Task 3 - empty function:

---
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ *alloc_grid - function that returns a pointer to a 2-D array of integers
+ *@width:
+ *@height:
+ *Return: grid or NULL if failure
+ */
+int **alloc_grid(int width, int height)
+{
+
+}
