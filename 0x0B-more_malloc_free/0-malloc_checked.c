0375653b3a5170335af4108e08f66ee0cdb7e5aa
Elaine Yeung <yeung.egs@gmail.com>
2017-03-09 22:59:34 +0000

    Add Task 0 - v1

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -2,12 +2,19 @@
 #include <stdlib.h>
 
 /**
- *main - allocate memory using malloc
+ *malloc_checked - allocate memory using malloc
  *@b: unsigned int
  *Return: a pointer to allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
+	void *p;
 
+	p = malloc(b);
+	if (p = NULL)
+	{
+		exit(98);
+	}
+	return (p);
 }
