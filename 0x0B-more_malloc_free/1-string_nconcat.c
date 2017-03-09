403904313ae1120eb40bc67145787f7075eb948b
Elaine Yeung <yeung.egs@gmail.com>
2017-03-09 23:49:47 +0000

    Add Task 1 - in progress version

--- 0x0B-more_malloc_free/1-string_nconcat.c
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdlib.h>
 /**
- *main - function to concatenate two strings
+ *string_nconcat - function to concatenate two strings
  *@s1: character string
  *@s2: character string
  *@n: unsigned integer
@@ -12,5 +12,41 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *a;
+	unsigned int i;
+	unsigned int j;
+	unsigned int c;
+	unsigned int d;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[j] != '\0'; j++)
+		;
+	j++;
+	if (n > j)
+	{
+		a = malloc((i * sizeof(*s1)) + (j * sizeof(*s2)) + 1);
+	}
+	else
+		a = malloc(i * sizeof(*a));
+	if (a == NULL)
+		return (NULL);
+	for (c = 0; c < i; c++)
+	{
+		;
+	}
+	for (d = 0; d < j; c++, d++)
+	{
+		;
+	}
+	*(a + i) = '\0';
+	return (a);
 }
