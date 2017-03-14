b212d5c094c883f0a125c405f16ae6fa06db1199
Elaine Yeung <yeung.egs@gmail.com>
2017-03-14 05:43:55 +0000

    Add Task 2 - in progress

---
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		d->name == NULL ? printf("(nil)\n";
+	}
+}
