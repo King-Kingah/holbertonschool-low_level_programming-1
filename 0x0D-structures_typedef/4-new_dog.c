163ffb58dcc5537cdb80911ad033f7b9ee62b5ab
Elaine Yeung <yeung.egs@gmail.com>
2017-03-14 06:55:18 +0000

    Add Task 4 - in progress

---
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - create a new dog
+ * @name: char string name
+ * @age: int age
+ * @owner: char string owner
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
