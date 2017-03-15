db6d3567c314533717b5ba158de10fd71d251731
Elaine Yeung <yeung.egs@gmail.com>
2017-03-15 20:22:37 +0000

    Add Task 0 - v1

---
+++ 0x0E-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+/**
+ * print_name - function to print name
+ * @name: character string
+ * @f: function pointer that takes character string arg
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
