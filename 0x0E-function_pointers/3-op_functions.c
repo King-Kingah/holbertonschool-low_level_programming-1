d0937b74cf435897ea05127a736c6a9351f40f33
Elaine Yeung <yeung.egs@gmail.com>
2017-03-15 21:55:05 +0000

    Add Task 3 - added function with operations

---
+++ 0x0E-function_pointers/3-op_functions.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - returns the sum of a and b.
+ * @a: integer 1
+ * @b: integer 2
+ * Return: sum of 2 integers
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - returns the difference of a and b.
+ * @a: integer 1
+ * @b: integer 2
+ * Return: difference of 2 integers
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - returns the product of a and b
+ * @a: integer 1
+ * @b: integer 2
+ * Return: product of 2 integers
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - returns the result of the division of a by b
+ * @a: integer 1
+ * @b: integer 2
+ * Return: dividend of 2 integers
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * @a: integer 1
+ * @b: integer 2
+ * Return: remainder of division of 2 integers
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("error\n");
+		exit(100);
+	}
+	return (a % b);
+}
