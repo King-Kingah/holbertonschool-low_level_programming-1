102d87b3f29659ca2fa48dfb46851366212cf625
Elaine Yeung <yeung.egs@gmail.com>
2017-03-15 22:07:57 +0000

    Add Task 3 - in progress

---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,13 @@
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argumemnt vector
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+}
