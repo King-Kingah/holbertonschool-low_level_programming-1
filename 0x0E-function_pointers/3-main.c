f29e6ea52856b581e24bf3ea72d2bd4bf9e3c7bd
Elaine Yeung <yeung.egs@gmail.com>
2017-03-15 22:22:33 +0000

    Add Task 3 - main function updated

--- 0x0E-function_pointers/3-main.c
+++ 0x0E-function_pointers/3-main.c
@@ -9,5 +9,25 @@
 
 int main(int argc, char *argv[])
 {
+	int a;
+	int b;
+	int (*op)(int a, int b);
 
+	if ()
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	op = get_op_func(argv[2]);
+	if (op == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	printf("%d", op(a,b));
 }
