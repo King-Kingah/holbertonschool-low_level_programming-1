5a68ab324a650745a192b6ee208493cb51b9ca7c
Elaine Yeung <yeung.egs@gmail.com>
2017-03-20 00:46:07 +0000

    Add Task 2 - add empty function

---
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_strings - print strings, then a new line
+ * @separator: char string used to separate strings
+ * @n: number of string args passed to function
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+
+}
