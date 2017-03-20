3d4555f5e00eb19301e72847145bdec52f391a5b
Elaine Yeung <yeung.egs@gmail.com>
2017-03-20 00:53:20 +0000

    Add Task 2 - work in progress

--- 0x0F-variadic_functions/2-print_strings.c
+++ 0x0F-variadic_functions/2-print_strings.c
@@ -8,5 +8,19 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list list;
 
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+
+	}
+	printf("\n");
+
+	va_end(list);
 }
