a83b6b08eaaef337c9727ca32cbbd204efac8a62
Elaine Yeung <yeung.egs@gmail.com>
2017-03-22 21:50:41 +0000

    Update function name

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -6,7 +6,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 void _printchar(va_list list);
-void _printstr(va_list list);
+void _printstring(va_list list);
 void _printfloat(va_list list);
 void _printint(va_list list);
 
