a2774568f81424cbc669484b5ad6b86d26a3dc29
Elaine Yeung <yeung.egs@gmail.com>
2017-03-24 21:16:52 +0000

    Add Task 5 with newline

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -5,5 +5,5 @@
 void __attribute__ ((constructor)) myConstructor()
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!");
+	printf("I bore my house upon my back!\n");
 }
