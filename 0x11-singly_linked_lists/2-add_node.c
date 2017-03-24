1fe8211c6ebb5427f269239d5b0680ce3746412b
Elaine Yeung <yeung.egs@gmail.com>
2017-03-24 06:45:01 +0000

    Add Task 2 - in progress

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -8,6 +8,26 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+	int i;
 
-return
+	for (i = 0; duplicate_str[c] != '\0'; i++)
+		;
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = i;
+	new_node-> = *head;
+
+	*head = new_node;
+	return (*head);
 }
