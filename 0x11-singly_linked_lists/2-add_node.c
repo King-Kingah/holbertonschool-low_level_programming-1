500744eada2a0713efd9bfaf8ccbf69ce6db524d
Elaine Yeung <yeung.egs@gmail.com>
2017-03-24 03:21:27 +0000

    Update Task 2 with function description

---
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ *add_node - function to add a node to the beginning of a list
+ *@head: current head of list
+ *@str: character string
+ *Return: pointer to a new node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+return
+}
