84ff9519edb6b8b58b9b3efb9d7efd7ab20a5a46
Elaine Yeung <yeung.egs@gmail.com>
2017-03-24 21:16:00 +0000

    Add Task 100 - fix function description

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
-* Apply the constructor attribute to myStartupFun() so that it
-* is executed before main()
+* __attribute__ ((constructor)) myConstructor - function to execute first
 */
-void myConstructor( void ) __attribute__ ((constructor))
+void __attribute__ ((constructor)) myConstructor()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!");
