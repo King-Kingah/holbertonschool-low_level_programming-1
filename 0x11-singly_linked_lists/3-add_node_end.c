da9c628348d88139d17587ffc095e6669c2f4ec9
Elaine Yeung <yeung.egs@gmail.com>
2017-03-24 03:24:49 +0000

    Add Task 3 - add function description

---
+++ 0x11-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * add_node_end - Add node to end of linked list
+ * @head: head of the list
+ * @str: string for node
+ *
+ * Return: address to new node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+}
