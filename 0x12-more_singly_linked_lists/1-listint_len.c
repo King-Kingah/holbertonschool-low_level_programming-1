8bdd6a33bb3d6ec4c244520b388a7ac4ba03b857
Elaine Yeung <yeung.egs@gmail.com>
2017-03-27 22:21:39 +0000

    Add Task 1 - v1

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 /**
- *
+ * listint_len -  function that returns the number of elements in a list
+ *@h: linked list head
+ *return: number of elements in list
  */
 size_t listint_len(const listint_t *h)
 {
