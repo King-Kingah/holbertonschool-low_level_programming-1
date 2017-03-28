2da8c03810c3d7b31887a1d459932b2f96717bb5
Elaine Yeung <yeung.egs@gmail.com>
2017-03-28 01:59:42 +0000

    Add Task 3 - fix formatting

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		current = current->next;
 
 	new = malloc(sizeof(listint_t));
-	if(new == NULL)
+	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
