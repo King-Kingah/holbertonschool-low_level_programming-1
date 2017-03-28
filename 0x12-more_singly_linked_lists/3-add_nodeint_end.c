4fc13d890d6c04d6d316632b3d37c1adf4516e04
Elaine Yeung <yeung.egs@gmail.com>
2017-03-28 01:58:55 +0000

    Add Task 3 - v1

--- 0x12-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x12-more_singly_linked_lists/3-add_nodeint_end.c
@@ -7,5 +7,22 @@
 */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
+	listint_t *current = NULL;
+	listint_t *new = NULL;
 
+	current = *head;
+	while (current && current->next != NULL)
+		current = current->next;
+
+	new = malloc(sizeof(listint_t));
+	if(new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+
+	if (current)
+		current->next = new;
+	else
+		*head = new;
+	return (new);
 }
