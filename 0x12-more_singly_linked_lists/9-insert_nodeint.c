c1c6845221907f316812cbdb5e353e11d0966d22
Elaine Yeung <yeung.egs@gmail.com>
2017-03-28 22:10:30 +0000

    Add Task 9 - v3 - fixed i initialization

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,7 +16,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	current = *head;
-	for (; current && i < idx; i++)
+	for (i = 1; current && i < idx; i++)
 	{
 		current = current->next;
 		if (current == NULL)
