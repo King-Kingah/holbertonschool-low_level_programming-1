ccb91f9e2a9c040ec7d258b2f33e59e3d5ee348a
Elaine Yeung <yeung.egs@gmail.com>
2017-03-28 02:48:10 +0000

    Add Task 5 - v1

--- 0x12-more_singly_linked_lists/5-free_listint2.c
+++ 0x12-more_singly_linked_lists/5-free_listint2.c
@@ -3,7 +3,18 @@
  * free_listint2 - function that frees a list and sets head to NULL
  * @head: pointer to head node
 */
+
 void free_listint2(listint_t **head)
 {
+	listint_t *space;
 
+	if (head == NULL)
+		return;
+
+	while (head != NULL)
+	{
+		space = head;
+		head = head->next;
+		free(space);
+	}
 }
