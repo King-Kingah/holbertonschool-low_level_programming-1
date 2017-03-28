aeeaaf4d9e6e15a25ce1976334d778db0649ea36
Elaine Yeung <yeung.egs@gmail.com>
2017-03-28 20:40:01 +0000

    Add Task 8 - v1

--- 0x12-more_singly_linked_lists/8-sum_listint.c
+++ 0x12-more_singly_linked_lists/8-sum_listint.c
@@ -6,5 +6,10 @@
  */
 int sum_listint(listint_t *head)
 {
+	int sum = 0;
+	listint_t *current;
 
+	for (current = head; current != NULL; current = current->next)
+		sum += current->n;
+	return (sum);
 }
