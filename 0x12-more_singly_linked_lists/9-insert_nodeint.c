dbd3bf1bc43a33ff9c5a263576f8009019775409
Elaine Yeung <yeung.egs@gmail.com>
2017-03-28 21:12:48 +0000

    Add Task 9 - v1

--- 0x12-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,5 +8,42 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	listint_t *new;
+	listint_t *current;
+	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
+
+	current = *head;
+	for (; current && i < idx; i++)
+	{
+		current = current->next;
+		if (current == NULL)
+			return (NULL);
+	}
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+	{
+		free (new);
+		return (NULL);
+	}
+	new->n = n;
+	if (idx == 0)
+	{
+		*head = new;
+		new->next = current;
+	}
+	else if (current->next)
+	{
+		new->next = current->next;
+		current->next = new;
+	}
+	else
+	{
+		new->next = NULL;
+		current->next = new;
+	}
+	return (new);
 }
