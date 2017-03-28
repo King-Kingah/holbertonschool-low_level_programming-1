51016832a7ef517ca5c736cca42a89eeb5e01519
Elaine Yeung <yeung.egs@gmail.com>
2017-03-28 20:00:00 +0000

    Add Task 7 - v1

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -8,5 +8,15 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
+	listint_t *current = head;
+	unsigned int i = 0;
 
+	while (current != NULL)
+	{
+		if (i == index)
+			return(current->n);
+		i++;
+		current = current->next;
+	}
+	return(i);
 }
