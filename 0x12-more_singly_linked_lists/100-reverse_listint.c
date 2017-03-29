a56045e7ea76e5d50ba5e6bbd081ff03588153bd
Elaine Yeung <yeung.egs@gmail.com>
2017-03-29 06:48:13 +0000

    Add empty files for advanced tasks

---
+++ 0x12-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * reverse_listint - Reverse a `listint_t` linked list
+ * @head: double pointer to head
+ * Description: Limited to going through loop only once.
+ * Not allowed to use `malloc` or `free` or arrays
+ * Can only declare a max of 2 variables.
+ * Return: pointer to first node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+}
---
+++ 0x12-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * free_listint_safe - free a `listint_t` list and set the head to null
+ * @h: double pointer to head of linked list
+ * Description: This function should work for circular lists
+ * Only loop through the list once
+ * Return: size of the list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+
+}
