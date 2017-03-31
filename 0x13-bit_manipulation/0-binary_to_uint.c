65bcb92a718bc7968447fbd396ca52fa59fc88ce
Elaine Yeung <yeung.egs@gmail.com>
2017-03-31 01:33:47 +0000

    Add Task 1 - v1

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,44 @@
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
-int get_endianness(void);
+#include "holberton.h"
+/**
+ * _strtou - convert string to unsigned int
+ * @c: char string
+ * Return: unsigned integer
+ */
+unsigned int _strtou(char c)
+{
+	return ((unsigned int) c - '0');
+}
+
+/**
+ *binary_to_uint - a function that converts a binary number to an unsigned int
+ *@b: const pointer to a string of 0 and 1 chars
+ *Return: the converted number, or 0 if any chars not 0 or 1 or b is NULL
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int convert = 0;
+	int i = 0;
+	int index = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		if (i == 1)
+		{
+			index = _strtou(b[i]);
+			convert = 0 * 2 + index;
+		}
+		else
+		{
+			index = _strtou(b[i]);
+			convert = convert * 2 + index;
+		}
+	}
+
+	return (convert);
+}
