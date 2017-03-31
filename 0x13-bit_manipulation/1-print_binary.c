784f09fe61ad7e82cd7243661f2dddf5897a5be0
Elaine Yeung <yeung.egs@gmail.com>
2017-03-31 02:33:42 +0000

    Add Task 1 - v1

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <limits.h>
 /**
  *print_binary - a function that prints the binary representation of a number
  *@n: unsigned long int
@@ -8,5 +9,16 @@
  */
 void print_binary(unsigned long int n)
 {
+	unsigned long int mask = 1u << ((sizeof(unsigned int) * CHAR_BIT) - 1);
+	int i;
+
+	for (i = 0; mask > 0; i++)
+	{
+		if ((n & mask) == 0)
+			_putchar('0');
+		else
+			_putchar('1');
+		mask = mask >> 1;
+	}
 
 }
