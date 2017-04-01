3697ed06b126f9b53bdde3282224336cf141e060
Elaine Yeung <yeung.egs@gmail.com>
2017-04-01 00:34:37 +0000

    Add Task 1 - PLD version

--- 0x13-bit_manipulation/1-print_binary.c
+++ 0x13-bit_manipulation/1-print_binary.c
@@ -9,18 +9,24 @@
  */
 void print_binary(unsigned long int n)
 {
-	bool index;
+	int index = 0;
 	int i;
 
-//	mask = ((sizeof(n) * 8) - 1);
 	if (n == 0)
-		_putchar('0');
-	for (i = ((sizeof(n) * 8) - 1); ; n; i--)
 	{
-		mask = mask >> 1;
-		if ((n & mask) == 0)
-			_putchar('0');
-		else
+		_putchar('0');
+	}
+
+	for (i = ((sizeof(n) * 8) - 1); i >= 0; i--)
+	{
+		if ((n >> i) & 1)
+		{
+			index = 1;
 			_putchar('1');
+		}
+		else if (index == 1)
+		{
+			_putchar('0');
+		}
 	}
 }
