71647f1e048388ea04d8e556de6f1d75f1ed5cfd
Elaine Yeung <yeung.egs@gmail.com>
2017-04-01 03:13:35 +0000

    Add Task 6 - v1 from PLD

--- 0x13-bit_manipulation/100-get_endianness.c
+++ 0x13-bit_manipulation/100-get_endianness.c
@@ -1,8 +1,15 @@
+#include "holberton.h"
 /**
  * get_endianness - a function that checks the endianness
  * Return: 0 if big endian, 1 if little endian
 */
 int get_endianness(void)
 {
+	unsigned int x;
+	char *c;
 
+	x = 1;
+	c = (char *) &x;
+
+	return (int)*c;
 }
