eec921ecc0c530434ac474fba501f4fbc078912a
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-08 14:02:52 -0700

    Update Task 2 to reflect new list item assignments

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+ #include "lists.h"
 /**
  *add_dnodeint - Write a function that adds a new node at the beginning of a dlistint_t list
  *@head: double pointer to head of list
@@ -17,8 +17,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	
 	/* put data into new node */
-	new->n = n;
-	
+       new->n = n;
+
+       /* assign new->next to point to head */
+       new->next = (*head);
+
+       /* assign head->prev to point to new */
+       head->prev = *new;
+       
+       /* assign new->prev to point to NULL */
+       new->prev = NULL;
+       
 	/* reassign head to point to new node */
 	(*head) = new;
 	
