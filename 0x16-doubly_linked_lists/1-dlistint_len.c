ba75c252ff3a93539391011163c43f04292b9f4b
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-08 11:15:23 -0700

    Add Task 1 - working version:

---
+++ 0x16-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ *dlistint_len - a function that returns the number of elements in a linked dlistint_t list
+ *@h: pointer to head of doubly linked list
+ *Return: number of elements in list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i;
+
+	for (i = 0; h != NULL; i++ )
+	{
+	}
+
+	return(n);
+}
