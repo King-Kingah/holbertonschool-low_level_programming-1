a0a3bdd6ef087178e7303f9451da4fd36b084a84
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-08 15:58:52 -0700

    Update Task 2 with Betty formatting

--- 0x16-doubly_linked_lists/2-add_dnodeint.c
+++ 0x16-doubly_linked_lists/2-add_dnodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- *add_dnodeint - Write a function that adds a new node at the beginning of a dlistint_t list
+ *add_dnodeint - adds a new node at the beginning of a dlistint_t list
  *@head: double pointer to head of list
  *@n: data stored in list
  *Return: the address of the new element, or NULL if it failed
@@ -15,19 +15,19 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	/* put data into new node */
-       new->n = n;
-       /* assign new->prev to point to NULL */
-       new->prev = NULL;
-       new->next = NULL;
+	new->n = n;
+	/* assign new->prev to point to NULL */
+	new->prev = NULL;
+	new->next = NULL;
 
-       if (*head != NULL)
-       {
-	       new->next = (*head); /* assign new->next to point to head */
-	       new->next->prev = new; /* assign head->prev to point to new */
-       }
+	if (*head != NULL)
+	{
+		new->next = (*head); /* assign new->next to point to head */
+		new->next->prev = new; /* assign head->prev to point to new */
+	}
 
-       /* reassign head to point to new node */
-       (*head) = new;
+	/* reassign head to point to new node */
+	(*head) = new;
 
 	return (new);
 }
