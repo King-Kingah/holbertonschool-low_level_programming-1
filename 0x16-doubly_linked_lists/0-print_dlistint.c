4bdc0b51c0850aa33d499c2131066895a76a17fd
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-08 10:59:20 -0700

    Update Task 0 - change variable type to size_t

--- 0x16-doubly_linked_lists/0-print_dlistint.c
+++ 0x16-doubly_linked_lists/0-print_dlistint.c
@@ -1,17 +1,17 @@
 #include "lists.h"
 /**
  *print_list - print all elements of a dlistint_t list
- *@h: linked list to print
+ *@h: pointer to head of doubly linked list
  *Return: formatted list to stdout
  */
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; h != NULL; i++)
 	{
-		printf("%s\n", h->next);
+		printf("%d\n", h->next);
 		h = h->next;
 	}
 	return (i);
