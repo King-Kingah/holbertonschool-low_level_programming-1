a3b1cadce286ce3d14076cf37030df28f87888d5
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-05-09 00:00:22 -0700

    Add Task 8 - singly list version

---
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - deletes the node at index index
+ * @head: double pointer to head
+ * @index: index of the list where the node to be deleted
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+int delete_dnodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *current = *head;
+	listint_t *new = NULL;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	for (; i < index; i++)
+	{
+		new = current;
+		if (current->next)
+			current = current->next;
+		else
+			return (-1);
+	}
+
+	if (index == 0)
+		*head = current->next; /* change head of list */
+	else if (current->next)
+		new->next = current->next; /* unlink node from linked list */
+	else
+		new->next = NULL; /* change end of list */
+
+	free(current); /* free memory of deleted node */
+
+	return (1);
+}
