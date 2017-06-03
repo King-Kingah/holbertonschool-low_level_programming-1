1cdb6ea164b4b459e456461173e0f30a720ba712
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 16:57:32 -0700

    Add Task 0 - v1

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -10,5 +10,21 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    
+	hash_table_t *new_ht;
+	
+	if (size == 0)
+		return (NULL);
+
+	new_ht = malloc(sizeof(hash_table_t));
+
+	if (new_ht == NULL)
+		return (NULL);
+
+	new_ht->size = size;
+	new_ht->array = malloc(sizeof(hash_node_t *));
+
+	if (new_ht->array == NULL)
+		return (NULL);
+
+	return (new_ht);
 }
