d57b0f85a673691b875ec34c3da4c0e96753e481
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 23:31:21 -0700

    Add Task 3 and 4 - passing local checks

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -4,16 +4,39 @@
  * hash_table_set - function that adds an element to the hash table
  * @ht: the hash table you want to add or update the key/value to
  * @key: the key
- * @value: the value associated with the key. 
+ * @value: the value associated with the key.
  *
  * Description:  key can not be an empty string, value can be an empty string
  *	In case of collision, add the new node at the beginning of the list
+ *	hash table already created using function from 0-hash_table_create
  * Return: 1 if it succeeded, 0 otherwise
  */
- 
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long index;
+	unsigned long size;
+	hash_node_t *new = NULL;
+
+	/* return 0 if table, key or value is NULL */
+	if (ht  == NULL || key == NULL || value == NULL)
+		return (0); /* return 0 on failure */
+
+	/* use key_index and determine size of table */
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+
+	/* allocate space for new node */
+	new = malloc(sizeof(hash_node_t));
+
+	if (new == NULL)
+		return (0); /* return 0 on failure */
+
+	/* assign data for new node */
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1); /* return 1 on success */
 }
-
-
-
--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -10,4 +10,29 @@
 
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long index;
+	hash_node_t *traverse;
+	int i;
+
+	/* return 0 if table, key or value is NULL */
+	if (ht  == NULL || key == NULL)
+		return (NULL); /* return NULL if key can't be found */
+
+	/* use key_index to determine index of the value */
+	index = key_index((const unsigned char *)key, ht->size);
+
+	traverse = ht->array[index];
+
+	if (traverse == NULL)
+		return (NULL);
+
+	/* handle collision instances */
+	for (i = strcmp(key, traverse->key); traverse != NULL; )
+	{
+		if (i == 0)
+			return (traverse->value);
+		traverse = traverse->next;
+	}
+
+	return (NULL);
 }
