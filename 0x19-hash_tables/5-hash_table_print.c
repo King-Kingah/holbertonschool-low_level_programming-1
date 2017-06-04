358828b3f81975ddc071328ae78186a042bfb0f4
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 23:53:03 -0700

    Add Task 5 - pseudocode mapped out

--- 0x19-hash_tables/5-hash_table_print.c
+++ 0x19-hash_tables/5-hash_table_print.c
@@ -12,4 +12,13 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	/* if table null, exit */
+
+	/* for loop - use two counters to iterate through
+	   hash table - one to go across, the other down */
+
+	/* if array at index exits then enter another for loop */
+
+	/* traverse through linked list of index */
+	/* at each node of linked list, print key/values to stdout */
 }
