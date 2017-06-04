a2c3fec36b35dc27034e52da1fdd092fd64ed6b1
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-03 17:34:08 -0700

    Task 0 - update second malloc

--- 0x19-hash_tables/0-hash_table_create.c
+++ 0x19-hash_tables/0-hash_table_create.c
@@ -11,20 +11,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht;
+	unsigned long int i;
 	
 	if (size == 0)
 		return (NULL);
 
+	/* allocate space for table */
 	new_ht = malloc(sizeof(hash_table_t));
 
 	if (new_ht == NULL)
 		return (NULL);
 
-	new_ht->size = size;
-	new_ht->array = malloc(sizeof(hash_node_t *));
+	/* allocate pointers to head nodes */
+	new_ht->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_ht->array == NULL)
 		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		new_ht->array[i] = NULL;
+	}
+	
+	new_ht->size = size;
 
 	return (new_ht);
 }
