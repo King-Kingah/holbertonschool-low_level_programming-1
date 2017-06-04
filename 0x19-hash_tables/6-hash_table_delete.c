c917358777e48f629ccdd0c1b777063db4998229
Elaine Yeung <yeungegs@users.noreply.github.com>
2017-06-04 01:49:17 -0700

    Add Task 6 - v1

--- 0x19-hash_tables/6-hash_table_delete.c
+++ 0x19-hash_tables/6-hash_table_delete.c
@@ -7,4 +7,20 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long i;
+	hash_node_t *traverse;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		traverse = ht->array[i];
+		while (traverse != NULL)
+		{
+			free(traverse->key);
+			free(traverse->value);
+			free(traverse);
+			traverse = traverse->next;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
